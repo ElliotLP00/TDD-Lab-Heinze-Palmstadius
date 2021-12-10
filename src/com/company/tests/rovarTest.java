70e56f2a5e01c9a2580308a1dbf44c6ef3093406
Elliot <Elliot.palmstadius@gmail.com>
2021-12-10 10:53:48 +0100

    sup

---
+++ src/com/company/tests/rovarTest.java
@@ -0,0 +1,23 @@
+package com.company.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class rovarTest {
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("new test");
+    }
+
+    @Test
+    void enrov() {
+
+    }
+
+    @Test
+    void derov() {
+    }
+}
